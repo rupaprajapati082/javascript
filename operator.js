1ac0cb463b76aee348173d37b24bbb7b35c4ff8d
rupaprajapati082 <rupsprajapati823@gmail.com>
2026-01-22 12:33:09 +0530

    ops

---
+++ operator.js
@@ -0,0 +1,102 @@
+// operators
+// Arithmetic, comparison, logical, assignment, unary, ternary
+
+// Arithmetic:
+// +, -, /, *, **, %
+
+// + -> Add and concatenation
+// ex. 1+2 -> Add
+// "user" + "name" -> concat
+// 5 + "5" -> concat, 5 + 5 -> Add
+
+// -
+// ex. 2 - 1
+
+// /, *
+// ex. 240/2, 25*2
+
+// % (modulus) (sheshfal) (reminder)
+// ex. 12%4 -> 0
+// ex. 9%2 -> 1
+
+// ** (square) (exposition)
+// ex. 2**3 -> 8
+
+// ---------------------------------------------------------------------------------------
+
+// Comparison (ans: true, false)
+
+// ==
+// ex. 12 == 13, 12 == "12" (don't check type, only check value, not strict comparison)
+
+// ===
+// ex. 12 === "12" (check type and value, strict comparaision)
+
+// !=
+// ex. 12 != 13 (reverse of ==, not strict comparision)
+
+// !==
+// ex. 12!== "12" (reverse of ===)
+
+// >=
+// ex. 22 >= 22
+
+// <=
+// ex. 13 <= 13
+
+// <
+// ex. 12 < 113, 12 < 9, 22 < 22
+
+// >
+// ex. 12 > 113, 12 > 9, 13 > 13
+
+
+// --------------------------------------------------------------------------------------------
+
+
+// assignment
+
+// =
+// give a value, value assignment operator
+// ex. let a = 12;
+
+// +=
+// ex. a += 3; (add value into old value and save it into a)
+
+// -=
+// ex. a -= 4; (subtract value into old value and save it into a)
+
+// *=
+// ex. a *= 2;
+
+// /=
+// ex. a /= 2;
+
+// %=
+// ex. a %= 2; (give remainder)
+
+// ---------------------------------------------------------------------------------------------------------
+
+
+// logical
+
+// && (AND)
+// ex. true && true --> true   12<13 && 14<15 --> true
+// false && true --> false   12<10 && 14 < 15 --> false
+// ture && false --> false
+// false && false --> false
+
+
+// || (OR)
+// ex. true || true --> true
+// false || true --> true
+// ture || false --> true
+// false || false --> false
+
+
+// ! (NOT)
+// ex. !true --> false
+// !false --> true
+// !12 --> false --> !!12 --> true
+
+// unary
