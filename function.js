b367de9391e153f2df5dcd902526d0655de33678
rupaprajapati082 <rupsprajapati823@gmail.com>
2026-02-03 12:46:52 +0530

    function

--- function.js
+++ function.js
@@ -110,3 +110,125 @@ console.log(result);
 //function can be passed as argument to other functions
 //functions can be returned from other functions
 
+//FUNSTION CAN BE PASSED AS ARGUMENT TO OTHER FUNCTIONS
+// let fnc = function(){}
+// fnc(function(){})
+function abc1(v1, v2){
+    console.log(v2);
+    return v1();
+}
+
+abc1(function(){
+    console.log("first class function")
+}, "Hello")
+
+function abc2(v1, v2){
+  
+    console.log(v2);
+      return v1();
+}
+
+abc2(function(){
+    console.log("first class function")
+}, "Hello")
+
+// function can be returned from other function
+function abc(){
+    return function (){
+        console.log("function within function");
+    }
+}
+abc()();   
+
+
+function abc2(){
+    return () => {
+        console.log("arrow function within function");
+    }
+}
+abc2()();  
+
+
+// higher order function (HOF)
+// function that takes another function as an argument or returns a
+// function as a result (eva function je return kare ek function athava
+// accept kare ek function params ma)
+
+// function abcd(val){val();}
+// --> abcd(function(){console.log("hello")})
+
+// function abcd(val){ }  --> higher order function
+
+// function abcd(){ return function(){} }  abcd()()  --> higher order
+//PURE VS IMPURE FUNCTIONS
+//PURE FUNCTIONS --> FUNCTION JE SAME INPUT PAR SAME OUTPUT APSE ANE KOI BAHAR NA STATE NE MODIFY NA KARE (PURE FUNCTION --> JE FUNCTION BHAR NA STATE NE MODIFY NA KARE)
+let a=20;
+function change_a(){
+    return "a not chnage(pure function);" +a;
+
+}//pure function
+
+console.log(change_a());
+//IMPURE FUNCTIONS --> FUNCTION JE SAME INPUT PAR SAME OUTPUT APSE ANE KOI BAHAR NA STATE NE MODIFY KARE IMPURE FUNCTION --> JE FUNCTION BHAR NA STATE NE MODIFY  KARE)
+
+function change_a1(){
+    a++;
+    a+=3;
+    return "a chnage(impure function)" +a;
+}
+console.log(change_a())
+console.log(change_a1())
+
+// closure function
+// function je potana parent function na variables ne access kari
+// shake (return thava valo function use karshe parent function na koi
+// variable) (function within function)
+
+function outer(){
+    let outer_var = "Outer Function Variable";
+    function inner(){
+        console.log(outer_var);
+    }
+
+
+inner();
+}
+outer();
+//lexical scope--> nested function can access variable in theri outer
+
+
+function outer1(){
+    let outer_var = "Outer Function Variable";
+    function inner1(){
+        console.log(outer_var);
+        let inner_var="inner function variable";
+        function most_inner(){
+          console.log(outer_var);
+            console.log(inner_var);
+    }
+    most_inner();
+}
+inner1();
+}
+outer1();
+//IIFE -Immediately invoked function expression
+(function (){})();//function je declare karta j call thai jay
+(function(){
+console.log("IIFE executed");
+})();
+//hoisting  in function 
+//use case--> show project structure
+temp_fnc();
+function temp_fnc(){
+    console.log("Hoisting in function")
+}
+
+let temp_fnc1=()=>{
+    console.log("Hoisting in arrow function ")
+};
+temp_fnc1();
+
+temp_fnc2();
+function temp_fnc2(){
+    console.log("Hoisting in function expression")
+};
